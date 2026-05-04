14fbb739ffe5050a06823aa2d833695936f653ff
Mathijs Slabbinck <mathijs.slabbinck@student-howest.be>
2026-05-04 21:43:01 +0200

    Add role seeder

--- Gotcha2.Core/Data/Seeder/Seeder.cs
+++ Gotcha2.Core/Data/Seeder/Seeder.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Gotcha2.Core.Constants;
+using Microsoft.AspNetCore.Identity;
 
 namespace Gotcha2.Core.Data.Seeder
 {
-    public class Seeder
+    // Idempotent startup seeder. Phase 1 only seeds the User role so that
+    // AuthController.Register's AddToRoleAsync(user, Roles.User) doesn't throw
+    // on the very first registration. Phase 3 will extend this class with SeedDataAsync
+    // for test users + a sample game.
+    public static class Seeder
     {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            bool exists = await roleManager.RoleExistsAsync(Roles.User);
+
+            if (!exists)
+            {
+                IdentityRole<Guid> role = new IdentityRole<Guid>(Roles.User);
+                await roleManager.CreateAsync(role);
+            }
+        }
     }
 }
