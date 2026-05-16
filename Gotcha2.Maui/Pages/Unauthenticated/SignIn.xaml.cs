e482e13e0357f817189f859a3958a125b4717501
Mathijs Slabbinck <mathijs.slabbinck@student.howest.be>
2026-05-16 17:04:37 +0200

    Wire SignIn.xaml.cs

--- Gotcha2.Maui/Pages/Unauthenticated/SignIn.xaml.cs
+++ Gotcha2.Maui/Pages/Unauthenticated/SignIn.xaml.cs
@@ -1,10 +1,18 @@
+using Gotcha2.Maui.ViewModels;
+
 namespace Gotcha2.Maui.Pages.Unauthenticated
 {
+    // partial because the XAML compiler generates a sibling partial that defines the named children (EmailEntry, PasswordEntry, etc.)
+    // plus the InitializeComponent() method.
+    // Your hand-written half + the generated half merge into one class at compile time
     public partial class SignIn : ContentPage
     {
-        public SignIn()
+        public SignIn(SignInViewModel viewModel)
         {
             InitializeComponent();
+
+            // wire the View to the ViewModel
+            BindingContext = viewModel;
         }
     }
 }
