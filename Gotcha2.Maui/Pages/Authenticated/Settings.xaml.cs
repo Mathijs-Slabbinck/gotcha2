e45517501ea13975b6b83c1a80e75f91e0a0a6ec
Mathijs Slabbinck <mathijs.slabbinck@student.howest.be>
2026-05-17 20:10:55 +0200

    Add initial seetings page & code behind

--- Gotcha2.Maui/Pages/Authenticated/Settings.xaml.cs
+++ Gotcha2.Maui/Pages/Authenticated/Settings.xaml.cs
@@ -1,10 +1,13 @@
+using Gotcha2.Maui.ViewModels;
+
 namespace Gotcha2.Maui.Pages.Authenticated
 {
     public partial class Settings : ContentPage
     {
-        public Settings()
+        public Settings(SettingsViewModel viewModel)
         {
             InitializeComponent();
+            BindingContext = viewModel;
         }
     }
 }
