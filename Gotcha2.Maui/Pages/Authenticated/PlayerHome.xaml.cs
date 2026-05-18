36697d8b4475bdf8359bb159c0d68f2dcfd52997
Mathijs Slabbinck <mathijs.slabbinck@student.howest.be>
2026-05-18 21:34:53 +0200

    Add Playerhome page

--- Gotcha2.Maui/Pages/Authenticated/PlayerHome.xaml.cs
+++ Gotcha2.Maui/Pages/Authenticated/PlayerHome.xaml.cs
@@ -1,10 +1,27 @@
+using Gotcha2.Maui.ViewModels;
+
 namespace Gotcha2.Maui.Pages.Authenticated
 {
     public partial class PlayerHome : ContentPage
     {
-        public PlayerHome()
+        private readonly PlayerHomeViewModel _viewModel;
+
+        public PlayerHome(PlayerHomeViewModel viewModel)
         {
             InitializeComponent();
+
+            _viewModel = viewModel;
+            BindingContext = _viewModel;
+        }
+
+        /* --- comment ---
+         * PlayerHome is pushed onto the Games tab nav stack
+         * Shell instantiates the page every time, so [QueryProperty] on the VM has GameId set by the time OnAppearing fires.
+         * OnAppearing (not the ctor) is also how we refresh when the user navigates back from ConfirmKill. */
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.LoadDataAsync();
         }
     }
 }
