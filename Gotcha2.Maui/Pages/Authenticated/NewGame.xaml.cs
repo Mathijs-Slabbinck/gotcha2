582be5da2217cfc6e725b65b0f35e6a10ecac243
Mathijs Slabbinck <mathijs.slabbinck@student.howest.be>
2026-05-18 18:25:46 +0200

    Add stubs

---
+++ Gotcha2.Maui/Pages/Authenticated/NewGame.xaml.cs
@@ -0,0 +1,10 @@
+namespace Gotcha2.Maui.Pages.Authenticated
+{
+    public partial class NewGame : ContentPage
+    {
+        public NewGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gotcha2.Maui/Pages/Authenticated/PlayerHome.xaml.cs
@@ -0,0 +1,10 @@
+namespace Gotcha2.Maui.Pages.Authenticated
+{
+    public partial class PlayerHome : ContentPage
+    {
+        public PlayerHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
