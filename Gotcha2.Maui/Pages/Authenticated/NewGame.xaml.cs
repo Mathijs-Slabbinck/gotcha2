4d613b0ec8da25f378b024f55dd868a6848ee8c8
Mathijs Slabbinck <mathijs.slabbinck@student.howest.be>
2026-05-19 19:25:41 +0200

    Wire newGameViewModel in NewGame.xaml.cs

--- Gotcha2.Maui/Pages/Authenticated/NewGame.xaml.cs
+++ Gotcha2.Maui/Pages/Authenticated/NewGame.xaml.cs
@@ -1,10 +1,15 @@
+using Gotcha2.Maui.ViewModels;
+
 namespace Gotcha2.Maui.Pages.Authenticated
 {
     public partial class NewGame : ContentPage
     {
-        public NewGame()
+        public NewGame(NewGameViewModel viewModel)
         {
             InitializeComponent();
+
+            // wire the View to the ViewModel
+            BindingContext = viewModel;
         }
     }
 }
